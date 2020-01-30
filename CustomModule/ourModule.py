4f5d5f658b5088a83f25093e963eb467e31efc35
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-01-30 09:19:24 -0700

    Organize Folder
    I moved things around just to be more organized

---
+++ CustomModule/ourModule.py
@@ -0,0 +1,33 @@
+
+
+
+
+def getNumbers(num1, num2):
+    results = num1 * num2
+    return results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# __name__ refers to THIS document's name
+# Python looks at this line first, aparently.
+# I thought this was an interpreted language that starts reading at the top of the documment though.
+if __name__ == "__main__":
+    pass
---
+++ CustomModule/printNum.py
@@ -0,0 +1,8 @@
+
+
+import ourModule
+
+
+
+if __name__ == "__main__":
+    print(ourModule.getNumbers(5, 9))
